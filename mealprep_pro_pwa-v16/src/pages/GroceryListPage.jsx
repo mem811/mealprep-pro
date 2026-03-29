b8ed374ecd10adb4900316747e9e377fbe53b0ba
Elise <hello@elisecellucci.com>
2026-03-29 10:17:01 -0400

    Fix style attribute syntax for progress bar width

--- mealprep_pro_pwa-v16/src/pages/GroceryListPage.jsx
+++ mealprep_pro_pwa-v16/src/pages/GroceryListPage.jsx
@@ -226,7 +226,7 @@ export default function GroceryListPage() {
         <div className="w-full bg-gray-100 rounded-full h-2">
           <div
             className="bg-green-500 h-2 rounded-full transition-all"
-            style= width: pct + '%' 
+            style= width: pct + '%'  
           ></div>
         </div>
       </div>
