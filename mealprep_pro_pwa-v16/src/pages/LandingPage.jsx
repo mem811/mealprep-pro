8ce504ba4fadf70dffb403b490cd4742daba02cf
Elise <hello@elisecellucci.com>
2026-04-10 11:19:31 -0400

    Update LandingPage.jsx

--- mealprep_pro_pwa-v16/src/pages/LandingPage.jsx
+++ mealprep_pro_pwa-v16/src/pages/LandingPage.jsx
@@ -1,4 +1,4 @@
-import PricingTable from '../components/landing/PricingTable'
+// import PricingTable from '../components/landing/PricingTable'
 
 export default function LandingPage() {
   return (
