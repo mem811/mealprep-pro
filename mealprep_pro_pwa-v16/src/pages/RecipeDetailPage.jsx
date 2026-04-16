a1f1521af7e5757aae78dce3261031143f77c300
Elise <hello@elisecellucci.com>
2026-04-16 08:48:11 -0400

    Update RecipeDetailPage.jsx

--- mealprep_pro_pwa-v16/src/pages/RecipeDetailPage.jsx
+++ mealprep_pro_pwa-v16/src/pages/RecipeDetailPage.jsx
@@ -551,8 +551,9 @@ export default function RecipeDetailPage() {
                 </button>
               );
             })}
-          </div>
-               
+      </div>
+      </div>
+
       {/* Delete Confirmation Modal */}
       {showDeleteModal && (
         <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/30 backdrop-blur-sm p-4">
@@ -579,6 +580,8 @@ export default function RecipeDetailPage() {
           </div>
         </div>
       )}
+
+      <div className="h-12 no-print" />
     </div>
   );
 }
